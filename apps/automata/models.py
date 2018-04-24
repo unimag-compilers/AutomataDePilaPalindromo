de4f02810cf5d9967cea4e2f269309b7d393c09e
Fabian Martinez <fabianmartinezmontenegro@gmail.com>
2018-04-24 01:34:57 -0500

    Se implemento la logica del automata impar

--- apps/automata/models.py
+++ apps/automata/models.py
@@ -20,3 +20,70 @@ class Expression(models.Model):
 	isPalindrome = models.CharField(max_length=1, choices=OPTIONS, default='unknown')
 
 	automata = models.ForeignKey(Automata, null=False, blank=False, on_delete=models.CASCADE)
+
+
+
+class AutomataPalindromoImpar:
+    pila = ['#']
+    tabla = [
+        ['f:', 'a', 'b', 'c', '~'],
+        [['p','#'], ['p','#a'], ['p','#b'], ['q','#'], ['','']],
+        [['p','a'], ['p','aa'], ['p','ab'], ['q','a'], ['','']],
+        [['p','b'], ['p','ba'], ['p','bb'], ['q','b'], ['','']],
+        [['q','#'], ['',''], ['',''], ['',''], ['r','#']],
+        [['q','a'], ['q','~'], ['',''], ['',''], ['','']],
+        [['q','b'], ['',''], ['q','~'], ['',''], ['','']],
+    ]
+    estadoActual = 'p'
+
+    def top(self):
+        return self.pila[-1]
+
+    def popPush(self, apilar):
+        self.pila.pop(-1)
+        for l in apilar:
+            if l != '~' :
+                self.pila.append(l)      
+
+    def validar(self, expresion):
+        nTransiciones = 0
+        cont = 0
+        for w in expresion :
+            cont = cont + 1
+            if self.estadoActual == 'p':            
+                for f in range(1, 4):
+                    if self.tabla[f][0][1] == self.top() :
+                        for c in range(1,4):
+                            if self.tabla[0][c] == w:
+                                self.estadoActual = self.tabla[f][c][0]
+                                self.popPush(self.tabla[f][c][1])
+                                #print ('letra',w,'- estado',self.tabla[f][c][0],'- pila',self.pila)
+                                nTransiciones = nTransiciones + 1
+                        if nTransiciones == cont: break
+            elif self.estadoActual == 'q':
+                for f in range(4, 7):
+                    if self.tabla[f][0][1] == self.top() :
+                        for c in range(1,5):
+                            if self.tabla[0][c] == w:
+                                self.estadoActual = self.tabla[f][c][0]
+                                self.popPush(self.tabla[f][c][1])
+                                #print ('letra',w,'- estado',self.tabla[f][c][0],'- pila',self.pila)
+                                nTransiciones = nTransiciones + 1
+                        if nTransiciones == cont: break
+        #endfor
+        if self.estadoActual == 'q' and self.top() == '#' and nTransiciones == len(expresion) :
+            self.popPush('#')
+            self.estadoActual = 'r'
+            #print(',#/#', self.pila)
+
+        if self.estadoActual == 'r' : 
+            self.returnToInitialState()
+            return True
+        else:
+            self.returnToInitialState()
+            return False
+    # fin funcion validar
+
+    def returnToInitialState(self):
+        self.estadoActual = 'p'
+        self.pila = ['#']	
--- apps/automata/views.py
+++ apps/automata/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-from apps.automata.models import Automata
+from apps.automata.models import Automata, AutomataPalindromoImpar
 
 def index(request):
 	return render(request, 'automata/index.html')
@@ -18,9 +18,15 @@ def automataImpar(request):
 	context = {"automata": aimpar}
 	return render(request, 'automata/automata-impar.html', context)
 
+
+automata_pi = AutomataPalindromoImpar()
+
 def automataImparEvaluar(request, expression):
 	aimpar = Automata.objects.filter(id=1)
-	context = {"automata": aimpar, "expression": expression}
+
+	result = automata_pi.validar(expression)
+
+	context = {"automata":aimpar, "expression":expression, "result":result}
 	return render(request, 'automata/automata-impar.html', context)
 
 def automataPar(request):
